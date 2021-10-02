349e46cf2e87f61f0cb10950492c5b65ed3ffcbc
Celso Taliatelli <celsotaliatelli@hotmail.com>
2021-10-02 10:59:55 -0300

    testing com debug

---
+++ src/components/PostCard/Post.spec.jsx
@@ -0,0 +1,17 @@
+import{ render } from '@testing-library/react';
+import { PostCard} from '.';
+
+const mock = {
+    title: 'title1',
+    body: 'body 1',
+    id:1,
+    cover: 'img/img.png'
+};
+
+
+describe('<PostCard />', () => {
+    it('should render PostCard correctly', () => {
+        const {debug} = render(<PostCard {...mock} />);
+        debug();
+    })
+})
